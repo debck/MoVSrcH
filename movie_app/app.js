e04659e3a72bf1a8b79d34b6655dd55c912aa499
debck <debasish.sahoo1998@gmail.com>
2018-05-12 17:08:54 +0530

    initial commit

---
+++ movie_app/app.js
@@ -0,0 +1,30 @@
+//setup =============================================================
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine","ejs");
+
+//routes ============================================================
+app.get("/", function(req,res){
+   res.render("search"); 
+});
+
+
+
+app.get("/results",function(req,res){
+    var moviename = req.query.movie;
+    var url ="http://omdbapi.com/?s="+moviename+"&apikey=thewdb";
+   request(url, function(error,response,body){
+     if(!error && response.statusCode == 200){ 
+         var data=JSON.parse(body)
+         res.render("result",{data: data});
+     }
+     else 
+        console.log(error);
+   }); 
+});
+
+//server ===========================================================
+app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("app has started!!!");
+});
